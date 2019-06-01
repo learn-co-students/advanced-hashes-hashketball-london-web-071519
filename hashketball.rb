9ee95747c5bde6e568752c1e7589369f316cb942
Eliot Howes <ejhowes@gmail.com>
2019-06-01 22:39:06 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => {
-        "Alan Anderson" => {:number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 1, :steals => 3, :blocks => 1, :slam_dunks => 1},
+        "Alan Anderson" => {:number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
         "Reggie Evans" => {:number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
         "Brook Lopez" => {:number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15},
         "Mason Plumlee" => {:number => 1, :shoe => 19, :points => 26, :rebounds => 12, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5},
@@ -20,7 +20,7 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => {
         "Jeff Adrien" => {:number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
-        "Bismack Biyombo" => {:number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 4, :blocks => 15, :slam_dunks => 10},
+        "Bismack Biyombo" => {:number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 7, :blocks => 15, :slam_dunks => 10},
         "DeSagna Diop" => {:number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5},
         "Ben Gordon" => {:number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0},
         "Brendan Haywood" => {:number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 22, :blocks => 5, :slam_dunks => 12},
@@ -107,4 +107,18 @@ def player_numbers(team_name)
       end
   end
   return players_number
+end
+
+def player_stats(player_name)
+  game_hash.each do |home_away, team_data|
+    team_data.each do |team, attributes|
+      if team == :players
+        attributes.each do |player, stats|
+          if player == player_name
+            return stats
+          end
+        end
+      end
+    end
+  end
 end
